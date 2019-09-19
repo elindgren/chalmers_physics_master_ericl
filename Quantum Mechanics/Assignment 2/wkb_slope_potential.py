d4e1a5ae2978bf4a19fb68cb42f4b289941044a8
Eric <ericlin@student.chalmers.se>
2019-09-19 22:19:10 +0200

    plot harmonic oscillator wave functions

--- Quantum Mechanics/Assignment 2/wkb_slope_potential.py
+++ Quantum Mechanics/Assignment 2/wkb_slope_potential.py
@@ -1,25 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from decimal import Decimal
 
+
+def energy_equation(energy, n):
+    tmp = np.sqrt(2 * m) / hbar * (-L / V0) * 2 / 3 * (energy ** (2 / 3) - (energy + V0) ** (2 / 3)) - (np.pi/4+np.pi*n)
+    #print(tmp)
+    return tmp
 
 # Define constants
 hbar = 1
 m = 1
 # Parameters
-V0 = 1
-a = 1
+V0 = 1  # 100 eV
 L = 1
 
 # Define energy space
-E = np.linspace(0, 10, 100)
+E = np.linspace(0, 100, 100)
 
-# Define function
-fun = np.sqrt(2*m)/hbar * (-L/V0) * 2/3 * (E**(2/3) - (E+V0)**(2/3))
 # Target points: pi/4 + n*pi
 n = np.array(range(20))  # 20 target points
-
 target = np.pi/4*np.ones((len(n),)) + np.pi*n
-# Plot function
 
+# Define function vector
+fun = np.zeros((len(target), 1))
+
+# Define plot
 fig, ax = plt.subplots()
-#ax.vline(target, [0,1], 'b')
+
+# Functions which are to be zerod
+for i in range(len(n)):
+    equation_n = np.zeros((len(E), 1))
+    for j in range(len(E)):
+        equation_n[j] = energy_equation(E[j], n[i])
+    # For each n, calculate fun - n*pi/2+pi/4, and find it's zero
+    # Solve equation
+    # TODO
+    # Plot function
+    ax.plot(E, equation_n, 'b')
+    # Plot solution to equation
+
+plt.xlabel("Energy, E")
+plt.ylabel("fun-n*pi/2+pi/4")
+plt.show()
+
+# TODO reduce to dimensionless constants
