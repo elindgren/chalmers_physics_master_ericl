25214882d529eb6ddd2b9213e4bf2008f7cc937d
Eric Lindgren <ericlin@student.chalmers.se>
2019-10-14 20:14:14 +0200

    Commit small changes

--- Quantum Mechanics/Assignment 3/Ahranov-Bohm.py
+++ Quantum Mechanics/Assignment 3/Ahranov-Bohm.py
@@ -1,19 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def energy_eig(l, j):
+def energy_eig(l, js):
     # Define j = q*B0*rho^2/2hbar
-    return (l-j)**2
+    E_vec = np.zeros(len(js))
+    for idx, j in enumerate(js):
+        #if not j < l:
+        E_vec[idx] = (l-j)**2
+
+    return E_vec
 
 # Constant in front of E: c = hbar^2/(2*m*rho^2)
 
-ls = np.arange(0,10)
-j = np.linspace(0,10,100)
+ls = np.arange(-4,4)
+j = np.linspace(-10,10,100)
 
 fig, ax = plt.subplots()
 
 for l in ls:
     ax.plot(j, energy_eig(l,j), label=f'l={l}')
 
+plt.grid()
 plt.legend()
 plt.show()
