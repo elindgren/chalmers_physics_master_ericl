4a26904852d53f135ea124e6bfd0e72abb6fab25
Eric Lindgren <ericlin@student.chalmers.se>
2019-11-08 19:52:18 +0100

    Start work on C exercise 2

---
+++ ComputationalPhysics/CIntro/c_2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+//#include  <gsl/gsl_rng.h>
+
+void problem1_scalar_product(double A[], double B[]){
+    printf("The product is %:.2f", 1);
+}
+
+int main(int argc){
+    printf("Input parameter %d", argc);
+    
+}
