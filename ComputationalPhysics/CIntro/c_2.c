bdf821f3343c5e801c3a53b2db6db45b6e0b59c5
Eric <ericlin@student.chalmers.se>
2019-11-08 21:59:17 +0100

    Finish first part of exercise 2

--- ComputationalPhysics/CIntro/c_2.c
+++ ComputationalPhysics/CIntro/c_2.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 #include <math.h>
-//#include  <gsl/gsl_rng.h>
+#include <gsl/gsl_blas.h>
 
-void problem1_scalar_product(double A[], double B[]){
-    printf("The product is %:.2f", 1);
+void problem1_scalar_product(gsl_vector A[], gsl_vector B[]){
+    double res = 0.0;
+    gsl_blas_ddot(A, B, &res);
+    printf("Result is: %.2f. \n", res);
 }
 
-int main(int argc){
-    printf("Input parameter %d", argc);
-    
+int main(){
+    int len = 0;
+    printf("Enter array length: ");
+    scanf("%d", &len);  // Unsigned int
+    printf("Scanned length: %d. \n", len);
+    // Declare arrays
+    gsl_vector *A; 
+    gsl_vector *B;
+    // Allocate memory
+    A = gsl_vector_alloc(len);
+    B = gsl_vector_alloc(len);
+    // Pre-allocate arrays
+    gsl_vector_set_all(A, 1.0);
+    gsl_vector_set_all(B, 2.0);
+    // Calculate scalar product
+    problem1_scalar_product(A,B);
 }
