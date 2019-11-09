f5a5ba53ac6107e3c40ce462c7b110b381ee600c
Eric <ericlin@student.chalmers.se>
2019-11-09 22:55:43 +0100

    Start last part of C-intro exercise 2

---
+++ ComputationalPhysics/CIntro/c2_functions.h
@@ -0,0 +1,3 @@
+void problem1_scalar_product(double A[], double B[], int len);
+
+void calc_distance(int p1, int p2, double ps[][3]);
--- ComputationalPhysics/CIntro/c_2.c
+++ ComputationalPhysics/CIntro/c_2.c
@@ -1,12 +1,8 @@
+# include "c2_functions.h"
 #include <stdio.h>
-#include <math.h>
-#include <gsl/gsl_blas.h>
+#include <stdlib.h>
+#include <gsl/gsl_rng.h>
 
-void problem1_scalar_product(gsl_vector A[], gsl_vector B[]){
-    double res = 0.0;
-    gsl_blas_ddot(A, B, &res);
-    printf("Result is: %.2f. \n", res);
-}
 
 int main(){
     int len = 0;
@@ -14,14 +10,56 @@ int main(){
     scanf("%d", &len);  // Unsigned int
     printf("Scanned length: %d. \n", len);
     // Declare arrays
-    gsl_vector *A; 
-    gsl_vector *B;
+    double * A; 
+    double * B;
     // Allocate memory
-    A = gsl_vector_alloc(len);
-    B = gsl_vector_alloc(len);
+    A = malloc(len*sizeof(double));
+    B = malloc(len*sizeof(double));
     // Pre-allocate arrays
-    gsl_vector_set_all(A, 1.0);
-    gsl_vector_set_all(B, 2.0);
+    for (int i=0; i<len; i=i+1){
+        A[i] = 1.0;
+        B[i] = 2.0;
+    }
     // Calculate scalar product
-    problem1_scalar_product(A,B);
-}
+    problem1_scalar_product(A,B, len);
+    // Do the same using VLAs
+    double A_vla[len];
+    double B_vla[len];
+    for (int i=0; i<len; i=i+1){
+        A_vla[i] = 1.0;
+        B_vla[i] = 2.0;
+    }
+    problem1_scalar_product(A_vla,B_vla, len);
+
+    // Calculate pointwise distances
+    double points[len][3];  // Switch directions, transpose matrix
+    // Declare positions
+    for (int i=0; i<len; i=i+1){
+        for (int j=0; j<3; j=j+1){
+            double val = (double)i;
+            points[i][j] = val;
+            //printf("Value: %.2f \n", points[i][j]);
+        }
+    }
+    // Calculate distance between points 0 and 1
+    calc_distance(0,2, points);
+    // Use gsl to create large array of uniformly drawn random numbers
+    gsl_rng *r = gsl_rng_alloc(gsl_rng_taus);
+    int size = 100000;
+    // Allocate vector
+    double *rand= malloc(size*sizeof(double));
+    for(int i=0; i<size; i++){
+        rand[i] = gsl_rng_uniform(r);
+    } 
+    // Define file to write to
+    char *filename = "uniform.txt";
+    FILE * fp;
+    fp = fopen(filename, "w");
+    // Write the vector to a file
+    for (int i=0; i<size; i++){
+        fprintf(fp, "%.2f\n", rand[i]);
+    }
+    // Free rng and vector
+    free(rand);
+    gsl_rng_free(r);
+}   
