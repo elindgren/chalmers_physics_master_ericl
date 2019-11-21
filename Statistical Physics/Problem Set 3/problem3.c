285bb3bb8902cf2d1984d639b68d15984bdc29ff
elindgren <ericlin@student.chalmers.se>
2019-11-21 17:43:37 +0100

    Sketch solution to problem3 pset3

---
+++ Statistical Physics/Problem Set 3/problem3.c
@@ -0,0 +1,19 @@
+
+
+double magnetization(int N, double lat[][N]){
+    // Calculation of average magnetization of lattice goes here
+}
+
+
+void metropolis(int N, double T, double kB, double lat[][N]){
+    // Implementation of metropolis algorithm goes here
+}
+
+void simulate_lattice(){
+    // Initialize lattice, run metropolis and save results
+}
+
+
+int main(){
+    // Define constants: J, kB
+}
