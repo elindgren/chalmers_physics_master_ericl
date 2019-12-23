c529088ec2079fb5ab0e04ca29bc4f75e40454f0
Eric Lindgren <ericlin@student.chalmers.se>
2019-12-23 15:04:42 +0100

    Start work on Problem Set 4

---
+++ Statistical Physics/Problem Set 4/problem1.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Define function
+
+def C(l, t, A, L, R, c):
+    k = 2*np.pi*l/L
+    print(k)
+    return l*(l+1)*A*L/k**2 * np.abs( np.cos( k * c * np.sqrt(1/(3*(1+R))) * t ) )**2 
+
+# Constants
+c = 1  # ly/y
+t = 380000  # 380000 years
+A = 1
+L = 9.7  # ly
+R = 0.7
+ls = np.linspace(40, 1500, 1000)
+
+# plot
+fig, ax = plt.subplots(figsize=(8,6))
+ax.plot(ls, C(ls, t, A, L, R, c))
+plt.show()
