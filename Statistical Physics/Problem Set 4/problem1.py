9b7187a0e9fb9d6f54b60fa5d276718e748a657c
Eric Lindgren <ericlin@student.chalmers.se>
2020-01-02 21:54:35 +0100

    Minor changes to problem 1

--- Statistical Physics/Problem Set 4/problem1.py
+++ Statistical Physics/Problem Set 4/problem1.py
@@ -5,15 +5,16 @@ import matplotlib.pyplot as plt
 
 def C(l, t, A, L, R, c):
     k = 2*np.pi*l/L
-    print(k)
-    return l*(l+1)*A/k**2 * np.abs( np.cos( k * c * np.sqrt(1/(3*(1+R))) * t ) )**2 
+    return l*(l+1)*A*L/k**2 * np.abs( np.cos( k * c * np.sqrt(1/(3*(1+R))) * t ) )**2 
 
 # Constants
 c = 1  # ly/y
 t = 380000  # 380000 years
 A = 1
-L = 9.7  # ly
+# L = 9.7  # ly
 R = 0.7
+L = 440 * c * t / np.sqrt((3*(1+R)))
+print(L)
 ls = np.linspace(40, 1500, 1000)
 
 # plot
