ede483e4c1b977ffa16ed2de1f4118f896593876
Eric Lindgren <ericlin@student.chalmers.se>
2020-01-06 11:32:20 +0100

    Finish problem set 7

--- Statistical Physics/Problem Set 4/problem1.py
+++ Statistical Physics/Problem Set 4/problem1.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 def C(l, t, A, L, R, c):
     k = 2*np.pi*l/L
-    return l*(l+1)*A*L/k**2 * np.abs( np.cos( k * c * np.sqrt(1/(3*(1+R))) * t ) )**2 
+    return l*(l+1)*A/k**2 * np.abs( np.cos( k * c * np.sqrt(1/(3*(1+R))) * t ) )**2 
 
 # Constants
 c = 1  # ly/y
@@ -14,7 +14,7 @@ A = 1
 # L = 9.7  # ly
 R = 0.7
 L = 440 * c * t / np.sqrt((3*(1+R)))
-print(L)
+print(f'L={L:.4f} ly')
 ls = np.linspace(40, 1500, 1000)
 
 # plot
