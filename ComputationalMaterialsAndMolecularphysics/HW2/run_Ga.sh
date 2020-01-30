21c34f494d8d8a3c653e138837ee32bda5c17f59
elindgren <ericlin@student.chalmers.se>
2020-01-30 07:11:35 +0100

    Run ga.py - unsuccesfully

---
+++ ComputationalMaterialsAndMolecularphysics/HW2/run_Ga.sh
@@ -0,0 +1,11 @@
+cd Na6
+python ../Na-clusters-GA-search/ga.py 
+echo ------------------ Na6 Done -------------------
+
+cd ../Na7
+python ../Na-clusters-GA-search/ga.py
+echo ------------------ Na7 Done -------------------
+
+cd ../Na8
+python ../Na-clusters-GA-search/ga.py
+echo ------------------ Na8 Done -------------------
