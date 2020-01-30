089eca2f1dae3ab6df607d0c9d531f3b98604329
Eric Lindgren <ericlin@student.chalmers.se>
2020-01-30 08:31:57 +0100

    Write hebbe script for HW2 ga.py

---
+++ ComputationalMaterialsAndMolecularphysics/HW2/hebbe_GA.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#SBATCH -p hebbe
+#SBATCH -A SNIC2020-5-51 # Project
+#SBATCH -J NaGA_ericlin # Name of the job
+#SBATCH -N 1 # Use 1 node
+#SBATCH -n 1 # Use only 1 core on that node
+#SBATCH -t 10:00:00 # Maximum time
+#SBATCH -o stdout_GA # stdout goes this file
+#SBATCH -e stderr_GA # stderr goes to this file
+
+module purge
+module load intel/2019a GPAW ASE
+
+cd Na6
+echo ------------------ Na6 Start ------------------
+mpirun -np 1 gpaw-python ../Na-clusters-GA-search/ga.py 
+echo ------------------ Na6 Done -------------------
+
+cd ../Na7
+echo ------------------ Na7 Start-------------------
+mpirun -np 1 gpaw-python ../Na-clusters-GA-search/ga.py
+echo ------------------ Na7 Done -------------------
+
+cd ../Na8
+echo ------------------ Na8 Start ------------------
+mpirun -np 1 gpaw-python ../Na-clusters-GA-search/ga.py
+echo ------------------ Na8 Done -------------------
