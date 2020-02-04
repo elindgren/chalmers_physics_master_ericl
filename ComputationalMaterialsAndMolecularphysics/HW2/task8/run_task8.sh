36b7779f48f87cc01b859cceecd5962b82b7df09
Eric Lindgren <ericlin@student.chalmers.se>
2020-02-04 16:21:16 +0100

    Finish run of task8 on Hebbe

--- ComputationalMaterialsAndMolecularphysics/HW2/task8/run_task8.sh
+++ ComputationalMaterialsAndMolecularphysics/HW2/task8/run_task8.sh
@@ -3,13 +3,13 @@
 #SBATCH -A SNIC2020-5-51 # Project
 #SBATCH -J task8_ericlin # Name of the job
 #SBATCH -N 1 # Use 1 node
-#SBATCH -n 10 # Use 10 cores on that node
-#SBATCH -t 15:00:00 # Maximum time
+#SBATCH -n 1 # Use 6 cores on that node
+#SBATCH -t 20:00:00 # Maximum time
 #SBATCH -o stdout_task8 # stdout goes this file
 #SBATCH -e stderr_task8 # stderr goes to this file
 
 module purge
-# module load intel/2019a GPAW ASE
+module load intel/2019a GPAW ASE
 module spider  GPAW/19.8.1-Python-3.7.2-libvdwxc
 module load GCC/8.2.0-2.31.1  OpenMPI/3.1.3
 module load  GPAW/19.8.1-Python-3.7.2-libvdwxc
