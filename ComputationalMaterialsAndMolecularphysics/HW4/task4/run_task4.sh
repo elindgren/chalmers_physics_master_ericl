c378cb602f2268909e02ad9b67319aab719134d6
Eric <ericlin@student.chalmers.se>
2020-02-24 10:58:18 +0100

    Update bash script for task 4

--- ComputationalMaterialsAndMolecularphysics/HW4/task4/run_task4.sh
+++ ComputationalMaterialsAndMolecularphysics/HW4/task4/run_task4.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #SBATCH -p hebbe
 #SBATCH -A SNIC2020-5-51 # Project
-#SBATCH -J TDDFT-1 # Name of the job
+#SBATCH -J TDDFT-4 # Name of the job
 #SBATCH -N 1 # Use 1 node
 #SBATCH -n 10 # Use 10 cores on that node
 #SBATCH -t 50:00:00 # Maximum time - expected time is 30-40 hrs
