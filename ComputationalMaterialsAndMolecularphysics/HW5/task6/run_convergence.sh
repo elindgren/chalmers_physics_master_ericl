410688f64c857046a52ae041459dab56389de5c5
Eric Lindgren <ericlin@student.chalmers.se>
2020-03-02 17:23:30 +0900

    Update convergence script

--- ComputationalMaterialsAndMolecularphysics/HW5/task6/run_convergence.sh
+++ ComputationalMaterialsAndMolecularphysics/HW5/task6/run_convergence.sh
@@ -3,7 +3,7 @@
 #SBATCH -A SNIC2020-5-51 # Project
 #SBATCH -J convergeBulk # Name of the job
 #SBATCH -N 1 # Use 1 node
-#SBATCH -n 1 # Use 10 cores on that node
+#SBATCH -n 10 # Use 10 cores on that node
 #SBATCH -t 50:00:00 # Maximum time - expected time is 30-40 hrs
 #SBATCH -o out_converge # stdout goes this file
 #SBATCH -e err_converge # stderr goes to this file
@@ -15,5 +15,5 @@ module purge
 module load intel/2019a GPAW ASE
 
 echo ------------------ Task 6 start ------------------
-mpirun -np=1 gpaw-python task6.py
+mpirun -np=10 gpaw-python task6.py
 echo ------------------ Task 6 done -------------------
