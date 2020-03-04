ff373f15a0acf2540e5293edb8acb016357203e6
Eric Lindgren <ericlin@student.chalmers.se>
2020-03-04 16:21:59 +0900

    Rest task 5 to use more memory

--- ComputationalMaterialsAndMolecularphysics/HW5/task5/run_EDOS.sh
+++ ComputationalMaterialsAndMolecularphysics/HW5/task5/run_EDOS.sh
@@ -3,7 +3,7 @@
 #SBATCH -A SNIC2020-5-51 # Project
 #SBATCH -J EDOS_nanoparticles # Name of the job
 #SBATCH -N 1 # Use 1 node
-#SBATCH -n 1 # Use 1 cores on that node
+#SBATCH -n 2 # Use 1 cores on that node
 #SBATCH -t 10:00:00 # Maximum time - expected time is 30-40 hrs
 #SBATCH -o out_edos # stdout goes this file
 #SBATCH -e err_edos # stderr goes to this file
@@ -15,5 +15,5 @@ module purge
 module load intel/2019a GPAW ASE
 
 echo ------------------ Task 5 start ------------------
-mpirun gpaw-python task5.py
+mpirun -np=1 gpaw-python task5.py
 echo ------------------ Task 5 done -------------------
