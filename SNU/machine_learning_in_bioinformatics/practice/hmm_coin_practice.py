b9ccde536e7fcf8024240884eeb471109ac19aa8
Eric Lindgren <ericlin@student.chalmers.se>
2020-03-31 20:40:06 +0900

    try to implement forward algorithm - fail

--- SNU/machine_learning_in_bioinformatics/practice/hmm_coin_practice.py
+++ SNU/machine_learning_in_bioinformatics/practice/hmm_coin_practice.py
@@ -88,23 +88,58 @@ def viterbi(x, e, a, a_0k):
             # l is the new state, either 0 (loaded) or 1 (fair). 0 is inaccessible p=0
             e_l = e_lx[l, :]
             v_a_kl = v[i-1,:] + a_kl[:, l]
-            v[i, l] =  e_l[xi] + np.max( v_a_kl )
+            v[i, l] =  e_l[xi] + np.max( v_a_kl ) # sufficient to max since log is monotonous
             pi[i-1] = np.argmax( v_a_kl ) - 1 # remove the extra state
     # Termination
     p_xpi = np.exp(np.max(v[-1,:]))
     # Traceback
     return pi
 
+def forward(x, e, a, a_0k):
+    ''' 
+        Given a sequence x, calculate the most probable state sequence pi.
+        Perform calculations in log space
+
+        Avoiding underflow: http://wittawat.com/posts/log-sum_exp_underflow.html
+    '''
+    # Initialization
+    f = np.zeros((len(x)+1, 3)) 
+    o = 0  # Almost 0, will be used as 0 here to avoid log errors
+    #          0  l  f
+    f[0, :] = [1, o, o]  # p of most probable path ending in each state, if it ends now
+    a_kl = np.array([ [o, a_0k[0], a_0k[1]], [o, 1-a[0], a[0]], [o, a[1], 1-a[1]] ])  # [ [a_00, a_0l, a_0f], [a_l0, a_ll, a_lf], [a_20, a_fl, a_ff] ]
+    e_lx = np.array([ [o, o], [e[0], 1-e[0]], [e[1], 1-e[1]] ])  # [ [e_0(h), e_0(t)], [e_l(h), e_l(t)], [e_f(h), e_f(t)] ]
+    # Convert to log space
+    f[0,:] = np.log(f[0,:])
+    a_kl = np.log(a_kl)
+    e_lx = np.log(e_lx)
+    # Recursion
+    for i in tqdm(range(1, len(x)+1)):
+        xi = x[i-1]
+        for l in range(f.shape[1]):
+            # l is the new state, either 0 (loaded) or 1 (fair). 0 is inaccessible p=0
+            e_l = e_lx[l, :]
+            f_a_kl = f[i-1,:] + a_kl[:, l]
+            c = np.max(f_a_kl)
+            print(np.sum(np.exp( f_a_kl )))
+            f[i, l] =  e_l[xi] + c + np.log(np.sum(np.exp( f_a_kl - c)))
+        print(f[i,:])
+        print('_____________')
+    # Termination
+    p_x = np.exp( np.sum( f[-1,:] ) )
+    # Traceback
+    print(p_x)
+    return p_x
+
 
 # Generate sequence of coin tosses
-L = 1000
+L = 2
 x, pi, e, a, a_0k = casino(L)
 
 # Obtain most probable state sequence - Viterbi algorithm
 pi_m = viterbi(x, e, a, a_0k)
 
 # Plot
-print(f'**** Viterbi accuracy: {(np.sum(pi_m == pi) / L):.2f}')
 fig, ax = plt.subplots(figsize=(8,6))
 ax.plot(pi_m[1:], c='C1', linewidth=2, alpha=0.7, label=r'$\pi^*$, Viterbi')
 # ax.fill_between(np.arange(L), 1, pi_m)
@@ -115,3 +150,12 @@ ax.set_title('Dishonest casino: Viterbi algorithm')
 ax.set_xlabel(r'$i$')
 ax.set_ylabel(r'Heads/tails')
 plt.savefig(f'viterbi_L={L}.png')
+
+# Obtain probability of sequence - Forward algorithm
+p_x = forward(x, e, a, a_0k)
+
+# Print results
+print()
+print('--------- Results ---------')
+print(f'**** Viterbi accuracy: {(np.sum(pi_m == pi) / L):.2f}')
+print(f'**** Forward: P(x): {p_x}')
