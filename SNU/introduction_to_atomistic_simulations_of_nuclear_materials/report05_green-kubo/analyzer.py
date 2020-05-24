Add temperature to report05 - todo, run with fix pressure and find missing factor 100

SNU/introduction_to_atomistic_simulations_of_nuclear_materials/report05_green-kubo/analyzer.py
@@ -15,10 +15,11 @@ plt.rc('legend', fontsize=14)    # legend fontsize
 line_cycler = cycler('linestyle',['-','--',':','-.', '-', '--']) + cycler('color', ['r', 'g', 'b', 'k', 'c', 'y'])
 plt.rc('axes', prop_cycle=line_cycler)
 
-
+#! TODO: Fix P or fix V?
 N = 6**3 * 4
 mAtom = 39.948 # u
 aMass = 1.66e-27 # kg
+kB = 1.38064852e-23 # m2*kg*s-2*K-1
 PP = {
     'dt': [],
     'xy': [],
@@ -54,6 +55,18 @@ with open('vol.output', 'r') as f:
         if(i>1):
             V.append(float(row.rstrip().split(' ')[1]))
 V = np.array( V )
+rho = N*mAtom*aMass / V[-1] * 1e3/(1e-8)**3 # units: g/cm^3
+print(f'Density: rho={rho:.3f} g/cm^3') 
+
+# read temperature
+T = []
+with open('temp.output', 'r') as f:
+    # Only final volume is interesting due to running average
+    for i, row in enumerate(f):
+        if(i>1):
+            T.append(float(row.rstrip().split(' ')[1]))
+T = np.array( T )
+
 
 # read pressure
 P = []
@@ -65,24 +78,29 @@ with open('press.output', 'r') as f:
 P = np.array( P )
 
 
-# Plot heatflux correlation functions
-
 # Calculate integral of heatflux function
 PP_trap = []
 for i in range(len(PP['dt'])):
     dt = PP['dt'][:i]
     PP_avg = PP['avg'][:i]
     PP_trap.append( np.trapz(PP_avg, dt) )
+PP_trap = np.array(PP_trap)
+T_f = T[-1]
+V_f = V[-1]
+L_eta = PP_trap * V_f / (kB*T_f) * 1e-25 # Convert to µg/cm*s
+# Calculate the viscosity
+eta = L_eta[-1]
+print(f'The viscosity is: {eta:.3f} µg/cm*s')
 
 # Plot --------------------
 fig, ax1 = plt.subplots(figsize=(8,6))
 ax2 = ax1.twinx()
-ax1.plot(V, linewidth=2, c='b', linestyle='-', label='V')
-ax1.set_ylabel(r'Volume ($\rm Å^3$)', color='b')
+ax1.plot(T, linewidth=2, c='b', linestyle='-', label='T')
+ax1.set_ylabel(r'Temperature $T$, (K)', color='b')
 ax1.set_xlabel(r'Dump iteration')
 
 ax2.plot(P, linewidth=2, c='r', linestyle='--', label='P')
-ax2.set_ylabel(r'Pressure (bar)', color='r')
+ax2.set_ylabel(r'Pressure $P$, (bar)', color='r')
 
 h1, l1 = ax1.get_legend_handles_labels()
 h2, l2 = ax2.get_legend_handles_labels()
@@ -97,11 +115,11 @@ plt.tight_layout()
 fig, ax1 = plt.subplots(figsize=(8,6))
 ax2 = ax1.twinx()
 ax1.plot(PP['dt'], PP['avg'], linewidth=2, c='b', linestyle='-', label=r'$\left< P(t)P(0) \right>$')
-ax1.set_ylabel(r'$\left< P(t)P(0) \right>$', color='b')
+ax1.set_ylabel(r'$\left< P(t)P(0) \right>$, ($\rm bar^2$)', color='b')
 ax1.set_xlabel(r'$t$, ps')
 
-ax2.plot(PP['dt'], PP_trap, linewidth=2, c='r', linestyle='--', label=r'$\int_0^t{\left< P(t)P(0) \right>}$')
-ax2.set_ylabel(r'$\int_0^t{\left< P(t)P(0) \right>}$', color='r')
+ax2.plot(PP['dt'], L_eta, linewidth=2, c='r', linestyle='--', label=r'$\eta$')
+ax2.set_ylabel(r'$\eta = \frac{V}{3kBT}\int_0^t{\left< P(s)P(0) \right>}ds$, (µg/cm s)', color='r')
 
 h1, l1 = ax1.get_legend_handles_labels()
 h2, l2 = ax2.get_legend_handles_labels()